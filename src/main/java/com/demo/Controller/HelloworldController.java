febf2a51dab1cfcf3293ed435f24aa9211ef4801
Suman Kumar <selfinfo.suman@gmail.com>
2025-06-17 19:18:27 +0530

    Updated message in HelloworldController

--- src/main/java/com/demo/Controller/HelloworldController.java
+++ src/main/java/com/demo/Controller/HelloworldController.java
@@ -1,10 +1,12 @@
 package com.demo.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloworldController {
 	
+	@GetMapping("/message")
 	public String getMessage() {
 		return "Hello-World";
 	}
